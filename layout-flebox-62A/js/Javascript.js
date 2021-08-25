685d1b96a7f9f5862c7663d7b54555b3b7cff085
Leonardo-Kotcheski-Filipiaki <81689412+Leonardo-Kotcheski-Filipiaki@users.noreply.github.com>
2021-08-24 22:39:38 -0300

    Add files via upload

---
+++ layout-flebox-62A/js/Javascript.js
@@ -0,0 +1,9 @@
+/*const btnmobile = document.getElementById('btnmobile')
+
+
+function toogleMenu() {
+    const navega = document.getElementById('navega');
+    navega.classList.toggle('active');
+}
+
+btnmobile.addEventListener('click', toggleMenu);*/
