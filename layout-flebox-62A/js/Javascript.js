5a967c805cacc6bf0822bb07d1eb066e545eecea
Leonardo-Kotcheski-Filipiaki <81689412+Leonardo-Kotcheski-Filipiaki@users.noreply.github.com>
2021-09-05 20:06:12 -0300

    Delete layout-flebox-62A directory

--- layout-flebox-62A/js/Javascript.js
+++
@@ -1,9 +0,0 @@
-/*const btnmobile = document.getElementById('btnmobile')
-
-
-function toogleMenu() {
-    const navega = document.getElementById('navega');
-    navega.classList.toggle('active');
-}
-
-btnmobile.addEventListener('click', toggleMenu);*/
